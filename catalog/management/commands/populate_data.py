82e25ea5dfcf0b03ad7c233531fb5ad63e7d3be7
WildJudge <wildjudge90@gmail.com>
2023-12-17 16:06:04 +0300

    Добавлено заполнение продуктов в populate_data.py

--- catalog/management/commands/populate_data.py
+++ catalog/management/commands/populate_data.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from catalog.models import Category
+from catalog.models import Category, Product
 
 
 class Command(BaseCommand):
@@ -8,10 +8,24 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # Очищаем таблицы перед заполнением
         Category.objects.all().delete()
+        Product.objects.all().delete()
 
-        # Заполняем данными
-        Category.objects.create(name='Electronics', description='Electronic gadgets')
-        Category.objects.create(name='Clothing', description='Fashion and clothing')
-        Category.objects.create(name='Books', description='Various books')
+        # Заполняем категории
+        electronics_category = Category.objects.create(name='Electronics', description='Electronic gadgets')
+        clothing_category = Category.objects.create(name='Clothing', description='Fashion and clothing')
+        books_category = Category.objects.create(name='Books', description='Various books')
+
+        # Заполняем продукты
+        Product.objects.create(name='Laptop', description='High-performance laptop', category=electronics_category,
+                               price=10000)
+        Product.objects.create(name='Smartphone', description='Latest smartphone model', category=electronics_category,
+                               price=1000)
+        Product.objects.create(name='T-shirt', description='Comfortable cotton T-shirt', category=clothing_category,
+                               price=300)
+        Product.objects.create(name='Jeans', description='Classic blue jeans', category=clothing_category, price=600)
+        Product.objects.create(name='Sci-Fi Book', description='Exciting science fiction novel',
+                               category=books_category, price=150)
+        Product.objects.create(name='Cookbook', description='Collection of delicious recipes', category=books_category,
+                               price=200)
 
         self.stdout.write(self.style.SUCCESS('Data successfully populated'))
